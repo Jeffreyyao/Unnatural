32f4a25b9a92be15d00d1758bdc55ed8549fa8cc
jeffreyyao <jeffrey@hestia.kitchen>
2026-08-26 15:01:45 +0800

    Initial Commit

---
+++ Unnatural/UnnaturalApp.swift
@@ -0,0 +1,17 @@
+//
+//  UnnaturalApp.swift
+//  Unnatural
+//
+//  Created by jeffrey on 26/8/2026.
+//
+
+import SwiftUI
+
+@main
+struct UnnaturalApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
